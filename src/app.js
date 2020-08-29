03d3c9d5cb083c122dc875210c860ae11ec2976b
hhbngo <58571870+hhbngo@users.noreply.github.com>
2020-08-29 12:24:39 -0500

    env.PORT fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const getResults = require('./utils/macro')
 const uniqid = require('uniqid')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
